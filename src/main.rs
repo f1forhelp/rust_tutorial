6a174f7a9a768afeb835e8caff8a01a52a760230
Utkarsh Sharma <rickysharma1111@gmail.com>
2024-10-05 22:54:13 +0530

    assa

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
 fn main() {
-    println!("Hello, world!");
+    //Ownership and Borrowing
+    // memories and heap tutorial [https://www.youtube.com/watch?v=_8-ht2AKyH4]
+
+    //------Ownership rules in Rust------
+    // 1. Each value in Rust has a variable that’s called its owner.
+    // 2. There can only be one owner at a time.
+    // 3. When the owner goes out of scope, the value will be dropped.
 }
